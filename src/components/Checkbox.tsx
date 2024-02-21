4fa5de5cf1e6a8b099a4feefcae101817a867393
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2024-02-21 16:49:15 +0000

    fix: Resolve console errors from SVG in Checkbox component.

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -12,7 +12,7 @@ export function Checkbox(p: CheckboxProps) {
                     className="peer h-5 w-5 cursor-pointer appearance-none rounded border border-gray-200 transition-all checked:border-gray-800 checked:bg-gray-800 hover:bg-gray-100 checked:hover:bg-gray-700"
                     id="checkbox"
                     checked={p.state}
-                    onClick={() => p.setState(!p.state)}
+                    onChange={() => p.setState(!p.state)}
                 />
                 <span className="pointer-events-none absolute p-1 text-white opacity-0 transition-opacity peer-checked:opacity-100">
                     <svg
@@ -21,12 +21,12 @@ export function Checkbox(p: CheckboxProps) {
                         viewBox="0 0 20 20"
                         fill="currentColor"
                         stroke="currentColor"
-                        stroke-width="1"
+                        strokeWidth="1"
                     >
                         <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                            clip-rule="evenodd"
+                            clipRule="evenodd"
                         ></path>
                     </svg>
                 </span>
