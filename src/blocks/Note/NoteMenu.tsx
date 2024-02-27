34d2aab5642f47baa6b699881f48f04fbc0f10b2
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2024-02-27 15:22:17 +0000

    feat!: Amend menu style and aria-expanded behaviour.

--- src/blocks/Note/NoteMenu.tsx
+++ src/blocks/Note/NoteMenu.tsx
@@ -1,10 +1,10 @@
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { noteService } from "../../database/noteService";
+import { MenuButton } from "../../components/MenuButton";
 import { NoteItem, NoteModifyableFields } from "../../database/database";
 import { noteThemes } from "../../utilities/noteThemes";
 import { NoteMenuTheme } from "./NoteMenuTheme";
-import { NoteMenuButton } from "./NoteMenuButton";
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
 import {
     FlagIcon,
@@ -39,20 +39,57 @@ export function NoteMenu(p: NoteMenuProps) {
         });
     }
 
-    const themes = Object.keys(noteThemes);
+    const menuThemes = Object.keys(noteThemes);
+
+    const menuOptions = [
+        {
+            icon: <CodeBracketIcon />,
+            label: "Monospace",
+            onClick: () =>
+                handleNoteModify({
+                    isMonospace: !p.noteData.isMonospace,
+                }),
+            isActive: p.noteData.isMonospace,
+            isDestructive: false,
+        },
+        {
+            icon: <FlagIcon />,
+            label: "Priority",
+            onClick: () =>
+                handleNoteModify({
+                    isPriority: !p.noteData.isPriority,
+                }),
+            isActive: p.noteData.isPriority,
+            isDestructive: false,
+        },
+        {
+            icon: <DocumentDuplicateIcon />,
+            label: "Duplicate",
+            onClick: () => handleNoteDuplicate(),
+            isActive: false,
+            isDestructive: false,
+        },
+        {
+            icon: <TrashIcon />,
+            label: "Delete",
+            onClick: () => handleNoteDelete(),
+            isActive: false,
+            isDestructive: true,
+        },
+    ];
 
     return (
         <Menu as="div" className="relative inline-block text-left">
             <Menu.Button
                 aria-label="Menu"
                 onClick={p.handleBringForwards}
-                className="px-4 py-2.5 hover:bg-gray-600/20 active:bg-gray-600/40"
+                className="px-4 py-2.5 hover:bg-gray-600/20 active:bg-gray-600/40 aria-expanded:bg-gray-600/20"
             >
                 <EllipsisHorizontalIcon className="h-4" />
             </Menu.Button>
             <Transition
                 as={Fragment}
-                enter="transition ease-out duration-100"
+                enter="transition ease-out duration-75"
                 enterFrom="transform opacity-0 scale-95"
                 enterTo="transform opacity-100 scale-100"
                 leave="transition ease-in duration-75"
@@ -60,59 +97,33 @@ export function NoteMenu(p: NoteMenuProps) {
                 leaveTo="transform opacity-0 scale-95"
             >
                 <Menu.Items className="absolute right-0 mr-2 mt-2 origin-top-right divide-y divide-gray-100 rounded bg-white shadow ring-1 ring-black/5 focus:outline-none">
-                    <div className="flex flex-col gap-1 px-1 py-1">
+                    <div className="flex flex-col px-1 py-1">
                         <Menu.Item as="div" className="flex gap-2 p-2">
-                            {themes.map((theme) => (
+                            {menuThemes.map((menuTheme) => (
                                 <NoteMenuTheme
-                                    key={theme}
+                                    key={menuTheme}
                                     onClick={() =>
-                                        handleNoteModify({ theme: theme })
+                                        handleNoteModify({ theme: menuTheme })
                                     }
-                                    themeId={theme}
-                                    themeIsActive={theme === p.noteData.theme}
+                                    themeId={menuTheme}
+                                    themeIsActive={
+                                        menuTheme === p.noteData.theme
+                                    }
                                 />
                             ))}
                         </Menu.Item>
-                        <hr />
-                        <Menu.Item>
-                            <NoteMenuButton
-                                onClick={() =>
-                                    handleNoteModify({
-                                        isMonospace: !p.noteData.isMonospace,
-                                    })
-                                }
-                                icon={<CodeBracketIcon />}
-                                label="Monospace"
-                                isActive={p.noteData.isMonospace}
-                            />
-                        </Menu.Item>
-                        <Menu.Item>
-                            <NoteMenuButton
-                                onClick={() =>
-                                    handleNoteModify({
-                                        isPriority: !p.noteData.isPriority,
-                                    })
-                                }
-                                icon={<FlagIcon />}
-                                label="Priority"
-                                isActive={p.noteData.isPriority}
-                            />
-                        </Menu.Item>
-                        <Menu.Item>
-                            <NoteMenuButton
-                                onClick={() => handleNoteDuplicate()}
-                                icon={<DocumentDuplicateIcon />}
-                                label="Duplicate"
-                            />
-                        </Menu.Item>
-                        <Menu.Item>
-                            <NoteMenuButton
-                                onClick={() => handleNoteDelete()}
-                                icon={<TrashIcon />}
-                                label="Delete"
-                                isDangerousAction={true}
-                            />
-                        </Menu.Item>
+                        <hr className="mb-1 border-gray-200"/>
+                        {menuOptions.map((menuOption) => (
+                            <Menu.Item key={menuOption.label}>
+                                <MenuButton
+                                    icon={menuOption.icon}
+                                    label={menuOption.label}
+                                    onClick={menuOption.onClick}
+                                    isActive={menuOption.isActive}
+                                    isDestructive={menuOption.isDestructive}
+                                />
+                            </Menu.Item>
+                        ))}
                     </div>
                 </Menu.Items>
             </Transition>
