c69e1b4cda46438be1f7ba75a037dd8790945e76
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2024-05-10 15:46:16 +0100

    fix: Add keys to TabGroup.

--- src/components/TabGroup.tsx
+++ src/components/TabGroup.tsx
@@ -20,6 +20,7 @@ export function TabGroup(p: TabGroupProps) {
             <TabList className="flex gap-6 border-b border-b-primary-100 dark:border-b-primary-700">
                 {p.items.map((item) => (
                     <Tab
+                        key={item.label}
                         disabled={item.isDisabled}
                         className="border-y-2 border-y-white/0 py-1.5 text-primary-500 active:border-b-primary-800 data-[hover]:border-b-primary-200 data-[selected]:border-b-primary-800 data-[hover]:text-primary-800 data-[selected]:text-primary-800 dark:text-primary-400 dark:active:border-b-primary-100 dark:data-[hover]:border-b-primary-400 dark:data-[selected]:border-b-primary-200 dark:data-[hover]:text-primary-200 dark:data-[selected]:text-primary-200"
                     >
@@ -29,7 +30,7 @@ export function TabGroup(p: TabGroupProps) {
             </TabList>
             <TabPanels>
                 {p.items.map((item) => (
-                    <TabPanel className="pt-4">{item.content}</TabPanel>
+                    <TabPanel key={item.label} className="pt-4">{item.content}</TabPanel>
                 ))}
             </TabPanels>
         </HTabGroup>
