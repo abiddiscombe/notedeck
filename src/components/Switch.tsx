38829db69c0dee4473aeff14eb7ffc0cd13c770f
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2024-07-22 12:55:07 +0100

    feat: Use green background for toggled ON Switch component.

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -12,11 +12,11 @@ export function Switch(p: SwitchProps) {
             checked={p.state}
             onChange={() => p.setState(!p.state)}
             className={twMerge(
-                "group inline-flex h-5 w-9 items-center rounded-full bg-primary-200 transition data-[checked]:bg-primary-800 data-[checked]:data-[hover]:bg-primary-700 data-[hover]:bg-primary-300 dark:bg-primary-600 dark:data-[checked]:bg-primary-100",
+                "group inline-flex h-5 w-9 items-center rounded-full bg-primary-200 transition data-[checked]:bg-green-500 data-[checked]:data-[hover]:bg-green-600 data-[hover]:bg-primary-300 dark:bg-primary-600 dark:data-[hover]:bg-primary-700",
                 p.className,
             )}
         >
-            <span className="size-3 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-5 dark:group-data-[checked]:bg-primary-800" />
+            <span className="size-3 translate-x-1 rounded-full bg-white transition group-data-[checked]:translate-x-5" />
         </HSwitch>
     );
 }
