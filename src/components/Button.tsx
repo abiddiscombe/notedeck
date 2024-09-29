5a53b8f79e214a1ea6b2b7032f256ebcb628b1a1
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2024-09-29 14:17:17 +0100

    chore: Amend code formatting styles to match Prettier defaults.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,37 +2,37 @@ import { twMerge } from "tailwind-merge";
 import { Button as _Button } from "@headlessui/react";
 
 const Button = (
-    p: React.ButtonHTMLAttributes<HTMLButtonElement> & {
-        size?: "sm" | "md" | "lg";
-        variant?: "solid" | "ghost" | "minimal" | "destructive";
-    },
+  p: React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    size?: "sm" | "md" | "lg";
+    variant?: "solid" | "ghost" | "minimal" | "destructive";
+  },
 ) => {
-    const size = p.size ?? "md";
-    const variant = p.variant ?? "minimal";
+  const size = p.size ?? "md";
+  const variant = p.variant ?? "minimal";
 
-    return (
-        <_Button
-            {...p}
-            className={twMerge(
-                `flex items-center rounded border disabled:pointer-events-none disabled:opacity-30
+  return (
+    <_Button
+      {...p}
+      className={twMerge(
+        `flex items-center rounded border disabled:pointer-events-none disabled:opacity-30
                 [&>svg]:h-3.5`,
-                size === "sm" && "h-7 gap-1.5 px-2.5 text-sm",
-                size === "md" && "h-8 gap-2.5 px-3 text-sm",
-                size === "lg" && "text-md h-10 gap-3 px-4",
-                variant === "solid" &&
-                    "border-primary-900 bg-primary-900 text-white data-[hover]:border-primary-700 data-[hover]:data-[active]:border-primary-600 data-[hover]:bg-primary-700 data-[hover]:data-[active]:bg-primary-600 dark:border-primary-200 dark:bg-primary-200 dark:text-primary-800 dark:data-[hover]:border-primary-300 dark:data-[hover]:data-[active]:border-primary-400 dark:data-[hover]:bg-primary-300 dark:data-[hover]:data-[active]:bg-primary-400",
-                variant === "ghost" &&
-                    "border-white/0 text-primary-800 data-[hover]:border-primary-100 data-[hover]:data-[active]:border-primary-200 data-[hover]:bg-primary-100 data-[hover]:data-[active]:bg-primary-200 dark:text-primary-200 dark:data-[hover]:border-primary-700 dark:data-[hover]:data-[active]:border-primary-600 dark:data-[hover]:bg-primary-700 dark:data-[hover]:data-[active]:bg-primary-600",
-                variant === "minimal" &&
-                    "border-primary-200 text-primary-800 data-[hover]:bg-primary-100 data-[hover]:data-[active]:bg-primary-200 data-[hover]:shadow-sm dark:border-primary-700 dark:text-primary-200 dark:data-[hover]:bg-primary-700 dark:data-[hover]:data-[active]:bg-primary-600",
-                variant === "destructive" &&
-                    "border-red-600 bg-red-600 text-white data-[hover]:border-red-500 data-[hover]:data-[active]:border-red-400 data-[hover]:bg-red-500 data-[hover]:data-[active]:bg-red-400 data-[hover]:shadow-sm",
-                p.className,
-            )}
-        >
-            {p.children}
-        </_Button>
-    );
+        size === "sm" && "h-7 gap-1.5 px-2.5 text-sm",
+        size === "md" && "h-8 gap-2.5 px-3 text-sm",
+        size === "lg" && "text-md h-10 gap-3 px-4",
+        variant === "solid" &&
+          "border-primary-900 bg-primary-900 text-white data-[hover]:border-primary-700 data-[hover]:data-[active]:border-primary-600 data-[hover]:bg-primary-700 data-[hover]:data-[active]:bg-primary-600 dark:border-primary-200 dark:bg-primary-200 dark:text-primary-800 dark:data-[hover]:border-primary-300 dark:data-[hover]:data-[active]:border-primary-400 dark:data-[hover]:bg-primary-300 dark:data-[hover]:data-[active]:bg-primary-400",
+        variant === "ghost" &&
+          "border-white/0 text-primary-800 data-[hover]:border-primary-100 data-[hover]:data-[active]:border-primary-200 data-[hover]:bg-primary-100 data-[hover]:data-[active]:bg-primary-200 dark:text-primary-200 dark:data-[hover]:border-primary-700 dark:data-[hover]:data-[active]:border-primary-600 dark:data-[hover]:bg-primary-700 dark:data-[hover]:data-[active]:bg-primary-600",
+        variant === "minimal" &&
+          "border-primary-200 text-primary-800 data-[hover]:bg-primary-100 data-[hover]:data-[active]:bg-primary-200 data-[hover]:shadow-sm dark:border-primary-700 dark:text-primary-200 dark:data-[hover]:bg-primary-700 dark:data-[hover]:data-[active]:bg-primary-600",
+        variant === "destructive" &&
+          "border-red-600 bg-red-600 text-white data-[hover]:border-red-500 data-[hover]:data-[active]:border-red-400 data-[hover]:bg-red-500 data-[hover]:data-[active]:bg-red-400 data-[hover]:shadow-sm",
+        p.className,
+      )}
+    >
+      {p.children}
+    </_Button>
+  );
 };
 
 export default Button;
--- src/utilities/backup.ts
+++ src/utilities/backup.ts
@@ -2,53 +2,53 @@ import { NoteItem } from "../database/db";
 import { notesService } from "../database/notes.service";
 
 export type BackupObject = {
-    id: "notedeck";
-    version: string;
-    timestamp: number;
-    content: {
-        notes: NoteItem[];
-    };
+  id: "notedeck";
+  version: string;
+  timestamp: number;
+  content: {
+    notes: NoteItem[];
+  };
 };
 
 const create = async () => {
-    const notes = await notesService.list();
+  const notes = await notesService.list();
 
-    // Represent the user's notes.
-    const backupContent: BackupObject = {
-        id: "notedeck",
-        version: APP_VERSION,
-        timestamp: Date.now(),
-        content: {
-            notes: notes,
-        },
-    };
+  // Represent the user's notes.
+  const backupContent: BackupObject = {
+    id: "notedeck",
+    version: APP_VERSION,
+    timestamp: Date.now(),
+    content: {
+      notes: notes,
+    },
+  };
 
-    return new Blob([JSON.stringify(backupContent)], {
-        type: "application/json",
-    });
+  return new Blob([JSON.stringify(backupContent)], {
+    type: "application/json",
+  });
 };
 
 const unpack = (content: string) => {
-    const parsed = JSON.parse(content);
+  const parsed = JSON.parse(content);
 
-    if (!parsed || !parsed.content || !parsed.timestamp) {
-        throw new Error(
-            "Failed to restore backup. One or more fields were missing.",
-        );
-    }
+  if (!parsed || !parsed.content || !parsed.timestamp) {
+    throw new Error(
+      "Failed to restore backup. One or more fields were missing.",
+    );
+  }
 
-    return parsed;
+  return parsed;
 };
 
 const restore = async (parsedBackup: BackupObject) => {
-    await notesService.removeAll(true);
-    parsedBackup.content.notes.forEach(async (note: NoteItem) => {
-        await notesService.create(note);
-    });
+  await notesService.removeAll(true);
+  parsedBackup.content.notes.forEach(async (note: NoteItem) => {
+    await notesService.create(note);
+  });
 };
 
 export const backup = {
-    create,
-    unpack,
-    restore,
+  create,
+  unpack,
+  restore,
 };
