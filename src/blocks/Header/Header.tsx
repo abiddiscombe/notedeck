55f61d639f6973860bb4e4753c4929b39dc2d903
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2025-09-09 21:54:00 +0100

    refactor: Improve Header logo structure.

--- src/blocks/Header/Header.tsx
+++ src/blocks/Header/Header.tsx
@@ -1,9 +1,9 @@
 import notes from "@/database/notes";
+import { APP_INFO } from "@/utilities/constants";
 import { useLiveQuery } from "dexie-react-hooks";
 import HeaderBackup from "./HeaderBackup";
 import HeaderDeleteNotes from "./HeaderDeleteNotes";
 import HeaderHideNonPriorityNotes from "./HeaderHideNonPriorityNotes";
-import HeaderLogo from "./HeaderLogo";
 import HeaderNewNote from "./HeaderNewNote";
 import HeaderSettings from "./HeaderSettings";
 import HeaderThemeSwitch from "./HeaderThemeSwitch";
@@ -11,19 +11,28 @@ import HeaderThemeSwitch from "./HeaderThemeSwitch";
 const Header = () => {
   const noteList = useLiveQuery(() => notes.list());
 
+  const userHasNotes = !!noteList?.length;
+
   return (
-    <header className="flex items-center gap-3 border-b border-b-neutral-200 bg-white px-4 py-1.5 shadow-xs dark:border-b-neutral-800 dark:bg-neutral-900">
-      <HeaderLogo />
-      {!!noteList?.length && (
-        <>
-          <HeaderNewNote />
-          <HeaderHideNonPriorityNotes />
-          <HeaderDeleteNotes />
-        </>
-      )}
-      <HeaderThemeSwitch />
-      <HeaderBackup />
-      <HeaderSettings />
+    <header className="flex items-center justify-between gap-3 border-b border-b-neutral-200 bg-white px-4 py-1.5 shadow-xs dark:border-b-neutral-800 dark:bg-neutral-900">
+      <span
+        id="logo"
+        className="text-sm font-semibold tracking-tight text-neutral-950 dark:text-neutral-100"
+      >
+        {APP_INFO.Name}
+      </span>
+      <nav className="flex items-center gap-2">
+        {userHasNotes && (
+          <>
+            <HeaderNewNote />
+            <HeaderHideNonPriorityNotes />
+            <HeaderDeleteNotes />
+          </>
+        )}
+        <HeaderThemeSwitch />
+        <HeaderBackup />
+        <HeaderSettings />
+      </nav>
     </header>
   );
 };
