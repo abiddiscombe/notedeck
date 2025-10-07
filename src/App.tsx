f04e23eaf5841b355e51ebe8d563214748cc26cd
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2025-10-07 18:27:17 +0100

    feat: implement CSS resets.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { isMobile } from "react-device-detect";
 import { Header } from "./blocks/Header/Header";
 import { Main } from "./blocks/Main/Main";
 import { Mobile } from "./blocks/Mobile/Mobile";
+import "./css/main.css";
 
 function App() {
   if (isMobile) {
@@ -9,7 +10,7 @@ function App() {
   }
 
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr] bg-neutral-50 dark:bg-neutral-950">
+    <div className="grid h-screen grid-rows-[auto_1fr]">
       <Header />
       <Main />
     </div>
