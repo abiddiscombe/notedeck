5bb66742646a14d01d58f791e92908bf2361f7a5
Archie Biddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2025-11-19 13:18:45 +0000

    fix: Correct broken GitHub link in mobile view. (#114)

--- src/providers/check-viewport.tsx
+++ src/providers/check-viewport.tsx
@@ -1,4 +1,4 @@
-import { Link } from "lucide-react";
+import { Link } from "@/components/elements/link";
 import React from "react";
 import { isMobile } from "react-device-detect";
 
@@ -11,19 +11,22 @@ export const CheckViewportProvider = ({
     return (
       <div className="bg-base-50 dark:bg-base-950 grid h-dvh grid-rows-[auto_1fr]">
         <header className="p-10">
-          <span className="text-base-950 dark:text-base-100 text-sm font-semibold tracking-tight">
+          <h1 className="text-base-950 dark:text-base-100 text-lg font-semibold tracking-tight">
             NoteDeck
-          </span>
+          </h1>
         </header>
         <main className="m-auto max-w-sm p-10 pb-20">
-          <h2>Sorry, mobile devices are not supported :/</h2>
-          <p>To use NoteDeck, open this page on your desktop or laptop.</p>
-          <p>
+          <h2 className="mb-6 text-xl">
+            Sorry, mobile devices are not supported <span aria-hidden>:/</span>
+          </h2>
+          <p className="mb-8">
+            To explore NoteDeck, open this page on your desktop or laptop.
+            {" "}
             <Link
               href="https://github.com/abiddiscombe/notedeck"
               target="_blank"
             >
-              Learn more
+              Learn more.
             </Link>
           </p>
         </main>
